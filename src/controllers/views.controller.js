f234d3facfd5d3ee66d48d80b78d65a1275d37a7
Luis <100473510+carlos8788@users.noreply.github.com>
2024-05-07 08:50:00 -0300

    schema users

--- src/controllers/views.controller.js
+++ src/controllers/views.controller.js
@@ -1,7 +1,5 @@
-import productService from "../services/product.service.js"
 
 export const dashboard = async (req, res) => {
-    const products = await productService.getProducts()
-    console.log(products)
-    res.render('dashboard', { products: products })
+    
+    res.render('dashboard',)
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import express from "express";
 import config from "./config.js";
 import connectToDB from "./connect.js";
 import productRoutes from "./routes/products.route.js"
+import userRoutes from "./routes/users.route.js"
 import viewRoutes from "./routes/views.route.js"
 import { engine } from "express-handlebars";
-
+import cors from "cors"
 const app = express()
-
+app.use(cors())
+app.use(express.static('public'))
+//Handlebars
 app.engine('handlebars', engine({
     runtimeOptions: {
       allowProtoPropertiesByDefault: true,
@@ -16,13 +19,16 @@ app.engine('handlebars', engine({
 app.set('view engine', 'handlebars')
 app.set('views', './src/views')
 
+// Conexión
 connectToDB()
 
+// JSON
 app.use(express.json())
 
 // Rutas
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 app.use('/', viewRoutes)
 
 
-app.listen(config.PORT, ()=> console.log('Listen to port '+ config.PORT))
+app.listen(config.PORT, ()=> console.log(`Listen to port ${config.PORT}`))
